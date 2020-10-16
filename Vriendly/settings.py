fbe1fa1b4e2dadb7a03e54f84bae4bc3d9546397
ZuuZaa <zumbaghirova@gmail.com>
2020-10-16 13:26:26 +0400

    data

--- Vriendly/settings.py
+++ Vriendly/settings.py
@@ -26,11 +26,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'x!=_=g)3kz4w*pt&+*h=32ku0=wnop
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 DATABASES = { 'default' : dj_database_url.config()}
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#     }
+# }
 
 ALLOWED_HOSTS = ['*']
 
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'Vriendly.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # DATABASES = {
