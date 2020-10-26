1016d0820249cf13e5f9d4333eac7aa29fb354f2
ZuuZaa <zumbaghirova@gmail.com>
2020-10-26 12:56:37 +0400

    wsgi

--- Vriendly/wsgi.py
+++ Vriendly/wsgi.py
@@ -8,13 +8,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
+#from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vriendly.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+#application = WhiteNoise(application)
+
+
+
 
 # from django.core.wsgi import get_wsgi_application 
 # from django.contrib.staticfiles.handlers import StaticFilesHandler
