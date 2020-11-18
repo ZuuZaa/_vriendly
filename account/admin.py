bd00cec0a68e186e011feb542dcd01328734a359
ZuuZaa <zumbaghirova@gmail.com>
2020-11-18 15:47:33 +0400

    account admin password

--- account/admin.py
+++ account/admin.py
@@ -6,13 +6,13 @@ from account.models import Account
 @admin.register(Account)
 class AccountAdmin(BaseUserAdmin):
 
-    list_display = ('email', 'first_name', 'last_name', 'status', 'date_of_birth', 'is_staff', 'is_active', 'is_superuser','last_login','last_logout')
+    list_display = ('email', 'first_name', 'last_name', 'status', 'date_of_birth', 'is_staff', 'is_active', 'is_superuser')
     list_filter = ('status',)
     readonly_fields = ('date_joined','last_login','last_logout')
 
     fieldsets = (
         
-        ('Personal info', {'fields': ('email', 'first_name', 'last_name', 'status', 'country', 'date_of_birth', 'password')}),
+        ('Personal info', {'fields': ('email', 'first_name', 'last_name', 'status', 'country', 'date_of_birth')}),
         ('Activity', {'fields': ('is_active', 'is_staff', 'is_superuser','last_login','last_logout')}),
         # ('Groups', {'fields': ('groups',)}),
         # ('Permissions', {'fields': ('user_permissions',)}),
