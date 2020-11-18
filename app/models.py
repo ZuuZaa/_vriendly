c9c7a273c6dce4e1a860288a08bf01b2a66296d5
ZuuZaa <zumbaghirova@gmail.com>
2020-11-18 15:41:25 +0400

    master ip

--- app/models.py
+++ app/models.py
@@ -3,15 +3,14 @@ from django.db import models
 # Create your models here.
 class Master_IP(models.Model):
 
-    name = models.CharField(max_length=200)
-    IP = models.CharField(max_length=200)
+    master_IP = models.CharField(max_length=200)
 
     class Meta:
         verbose_name = 'Master IP'
         verbose_name_plural = 'Master IP'
-        
+
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.master_IP}"
 
 class MyApp(models.Model):
 
